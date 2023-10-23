d267f1d443e219aa774cebd3bc536a542b15b672
21o1max <148799421+21o1max@users.noreply.github.com>
2023-10-23 18:52:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
+global.owner = [Lamilotn
+  ['2348122501507', 'Fortune', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'Lamil mafia'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
